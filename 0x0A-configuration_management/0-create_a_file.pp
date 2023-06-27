6d41be7ab31bcff7ebbc1acee5e7af7b629ae6da
Godswill Utong <godswillutong@gmail.com>
2023-06-27 10:39:38 +0000

    fixed errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # creates a file 'school' in /tmp
 file { '/tmp/school':
     ensure  => present,
+    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
-    mode    => '0744',
-    content => 'I love school',
+    content => 'I love Puppet',
 }
 
